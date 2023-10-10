492ee47546354baf4a1d58f5d6af0c483f80cb34
Emma Stott <emmastott@gmail.com>
2023-10-10 15:22:35 +0100

    feat: Use fontawesome instead of feather.

--- themes/emmastott/assets/js/main.js
+++ themes/emmastott/assets/js/main.js
@@ -1,22 +1,18 @@
-function toggleTheme() {
-	let currentTheme = document.documentElement.getAttribute("data-theme");
-	let targetTheme = currentTheme === "dark" ? "light" : "dark";
+function toggleDarkMode() {
+	let currentMode = document.documentElement.getAttribute("data-theme");
+	let targetMode = currentMode === "dark" ? "light" : "dark";
 
-	document.documentElement.setAttribute("data-theme", targetTheme)
-	localStorage.setItem("theme", targetTheme);
-
-	document.getElementById("theme-toggle").dataset.feather = targetTheme === "dark" ? "moon" : "sun";
-	feather.replace();
-	document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
+	document.documentElement.setAttribute("data-theme", targetMode)
+	localStorage.setItem("theme", targetMode);
 };
 
-let storedTheme = localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-if (storedTheme) {
-	document.documentElement.setAttribute("data-theme", storedTheme)
+let storedMode = localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedMode) {
+	document.documentElement.setAttribute("data-theme", storedMode)
 }
 
 document.onreadystatechange = () => {
 	if (document.readyState === "complete") {
-		document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
+		document.getElementById("darkmode-toggle").addEventListener("click", toggleDarkMode);
 	}
 };
